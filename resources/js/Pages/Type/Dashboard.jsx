796c03d3c3fd293dcf118309896ea704687f2a9a
RobieeInt <74244073+RobieeInt@users.noreply.github.com>
2022-07-10 17:21:10 +0700

    Slicing Nonton Component

--- resources/js/Pages/Type/Dashboard.jsx
+++ resources/js/Pages/Type/Dashboard.jsx
@@ -1,6 +1,8 @@
 import Authenticated from "@/Layouts/Authenticated/Index";
 import { Head } from "@inertiajs/inertia-react";
 import Flickity from "react-flickity-component";
+import FeaturedMovie from "@/Components/FeaturedMovie";
+import CardMovie from "@/Components/CardMovie";
 
 export default function Dashboard() {
     const flickityOptions = {
@@ -28,51 +30,33 @@ export default function Dashboard() {
                     </div>
                     <Flickity className="gap-[30px]" options={flickityOptions}>
                         {[1, 2, 3, 4].map((i) => (
-                            <div
+                            <FeaturedMovie
                                 key={i}
-                                className="absolute overflow-hidden group mr-[30px]"
-                            >
-                                {/* <!-- Movie Thumbnail --> */}
-                                <img
-                                    src="/images/featured-1.png"
-                                    className="object-cover rounded-[30px] w-[520px] h-[340px]"
-                                    alt=""
-                                />
-                                {/* <!-- rating --> */}
-                                <div className="rating absolute top-0 left-0">
-                                    <div className="p-[30px] flex items-center gap-1">
-                                        <img src="/icons/ic_star.svg" alt="" />
-                                        <span className="text-sm font-medium text-white mt-1">
-                                            4.5/5.0
-                                        </span>
-                                    </div>
-                                </div>
-                                {/* <!-- bottom detail --> */}
-                                <div
-                                    className="absolute bottom-0 h-[100px] left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px]
-                                rounded-br-[28px] flex justify-between items-center px-7 h-[130px]"
-                                >
-                                    <div>
-                                        <div className="font-medium text-[22px] text-white">
-                                            The Batman in Love
-                                        </div>
-                                        <p className="mb-0 text-white text-sm font-light">
-                                            Action • Horror
-                                        </p>
-                                    </div>
-                                    <div className="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
-                                        <img
-                                            src="/icons/ic_play.svg"
-                                            width="50"
-                                            alt=""
-                                        />
-                                    </div>
-                                </div>
-                                <a
-                                    href="watching.html"
-                                    className="inset-0 absolute z-50"
-                                ></a>
-                            </div>
+                                slug="Arsya Movie"
+                                title={`Arsya Movie ${i}`}
+                                genres="Comedy"
+                                thumbnail="https://picsum.photos/200"
+                                rating={i + 1}
+                                year={2019 + i}
+                            />
+                        ))}
+                    </Flickity>
+                </div>
+                <div className="mt-[50px]">
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Browse
+                    </div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+                            <CardMovie
+                                key={i}
+                                slug="Arsya"
+                                title={`Nigga ${i}`}
+                                genres="Comedy"
+                                thumbnail="https://picsum.photos/200/300"
+                                rating={i + 1}
+                                year={2029 + i}
+                            />
                         ))}
                     </Flickity>
                 </div>
